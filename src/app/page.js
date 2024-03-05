8e293b25419782f3b2201f2b3f2116fece71035d
YangYue128-helen <yangyue1228@hotmail.com>
2024-03-05 18:38:29 +0800

    feat(analysis): add iframe in home page

--- src/app/page.js
+++ src/app/page.js
@@ -12,6 +12,16 @@ export default function Home() {
     <>
       <Heading className="mb-5">Welcome to WMS</Heading>
       <Link href="/warehouse">Getting Started</Link>
+      <div className="mt-10 flex items-center justify-center">
+        <iframe
+          title="warehouse"
+          width="1800"
+          height="1120.5"
+          src="https://app.powerbi.com/view?r=eyJrIjoiMzM1MjM5MmUtODY2OC00MzZiLWIyZDQtOTdiYmEwMzdlNTQwIiwidCI6ImFhNGU1ODM1LWU3YjctNDQ3NC1hZTE1LWQ3OTA0OTYwZDY2NCIsImMiOjEwfQ%3D%3D"
+          frameborder="0"
+          allowFullScreen="true"
+        ></iframe>
+      </div>
     </>
   );
 }
