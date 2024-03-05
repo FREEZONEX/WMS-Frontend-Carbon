5ef3d4fd4b2e2d010b1dd4511a94371b8522ddb2
YangYue128-helen <yangyue1228@hotmail.com>
2024-03-05 18:04:02 +0800

    feat(home): home page

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,17 @@
 'use client';
-import { Button } from '@carbon/react';
+import React, { useEffect } from 'react';
+import { Button, Heading, Link } from '@carbon/react';
+import { useRouter } from 'next/navigation';
 
 export default function Home() {
-  return <Button className="button-primary">Button</Button>;
+  // const router = useRouter();
+  // useEffect(() => {
+  //   router.push('/warehouse');
+  // }, [router]);
+  return (
+    <>
+      <Heading className="mb-5">Welcome to WMS</Heading>
+      <Link href="/warehouse">Getting Started</Link>
+    </>
+  );
 }
