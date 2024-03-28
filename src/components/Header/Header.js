8db5704403ae220aeeac3c208ba38257808769a0
YangYue128-helen <yangyue1228@hotmail.com>
2024-03-28 17:33:55 +0800

    fix(bug): bootstrap remove

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -13,6 +13,7 @@ import {
   SideNavLink,
   SwitcherDivider,
   Toggle,
+  Theme,
 } from '@carbon/react';
 
 import {
@@ -24,7 +25,6 @@ import {
   User,
   Information,
   Settings,
-  Help,
   Analytics,
   WatsonHealth3DCursor,
 } from '@carbon/icons-react';
@@ -50,91 +50,94 @@ export const HeaderWSideNav = ({
         onClick={toggleSideNavExpanded}
         isActive={isSideNavExpanded}
         aria-expanded={isSideNavExpanded}
-      />{' '}
-      <a
+      />
+      <HeaderName
+        prefix="SUPCON"
         onClick={() => {
           router.push(`${process.env.PATH_PREFIX}/`);
         }}
         className="cursor-pointer"
       >
-        <HeaderName prefix="SUPCON">WMS</HeaderName>
-      </a>
-      <SideNav aria-label="Side navigation" expanded={isSideNavExpanded}>
-        <SideNavItems>
-          <SideNavLink
-            onClick={() => {
-              router.push(`${process.env.PATH_PREFIX}/warehouse`);
-            }}
-            className="cursor-pointer"
-            isActive={isCurrentPath('/warehouse')}
-          >
-            <IbmDb2Warehouse className="mr-[1.5rem]" />
-            <span>Warehouse</span>
-          </SideNavLink>
-          <SideNavLink
-            renderIcon={Product}
-            onClick={() => {
-              router.push(`${process.env.PATH_PREFIX}/warehouse/material`);
-            }}
-            className="cursor-pointer"
-            isActive={isCurrentPath('/warehouse/material')}
-          >
-            Material
-          </SideNavLink>
-          <SwitcherDivider />
-          <SideNavLink
-            renderIcon={PortInput}
-            onClick={() => {
-              router.push(`${process.env.PATH_PREFIX}/operation/inbound`);
-            }}
-            className="cursor-pointer"
-            isActive={isCurrentPath('/operation/inbound')}
-          >
-            Inbound
-          </SideNavLink>
-          <SideNavLink
-            renderIcon={PortOutput}
-            onClick={() => {
-              router.push(`${process.env.PATH_PREFIX}/operation/outbound`);
-            }}
-            className="cursor-pointer"
-            isActive={isCurrentPath('/operation/outbound')}
-          >
-            Outbound
-          </SideNavLink>
-          <SideNavLink
-            renderIcon={InventoryManagement}
-            onClick={() => {
-              router.push(`${process.env.PATH_PREFIX}/operation/stocktaking`);
-            }}
-            className="cursor-pointer"
-            isActive={isCurrentPath('/operation/stocktaking')}
-          >
-            Stocktaking
-          </SideNavLink>
-          <SwitcherDivider />
-          <SideNavLink
-            renderIcon={Analytics}
-            onClick={() => {
-              router.push(`${process.env.PATH_PREFIX}/analysis`);
-            }}
-            className="cursor-pointer"
-            isActive={isCurrentPath('/analysis')}
-          >
-            Analysis
-          </SideNavLink>
-          <SideNavLink
-            renderIcon={WatsonHealth3DCursor}
-            onClick={() => {
-              router.push(`${process.env.PATH_PREFIX}/analysis/3d`);
-            }}
-            className="cursor-pointer"
-            isActive={isCurrentPath('/analysis/3d')}
-          >
-            3D-Modeling
-          </SideNavLink>
-        </SideNavItems>
-      </SideNav>
+        WMS
+      </HeaderName>
+      <Theme theme={theme.sideNavTheme}>
+        <SideNav aria-label="Side navigation" expanded={isSideNavExpanded}>
+          <SideNavItems>
+            <SideNavLink
+              onClick={() => {
+                router.push(`${process.env.PATH_PREFIX}/warehouse`);
+              }}
+              className="cursor-pointer"
+              isActive={isCurrentPath('/warehouse')}
+            >
+              <IbmDb2Warehouse className="mr-[1.5rem]" />
+              <span>Warehouse</span>
+            </SideNavLink>
+            <SideNavLink
+              renderIcon={Product}
+              onClick={() => {
+                router.push(`${process.env.PATH_PREFIX}/warehouse/material`);
+              }}
+              className="cursor-pointer"
+              isActive={isCurrentPath('/warehouse/material')}
+            >
+              Material
+            </SideNavLink>
+            <SwitcherDivider />
+            <SideNavLink
+              renderIcon={PortInput}
+              onClick={() => {
+                router.push(`${process.env.PATH_PREFIX}/operation/inbound`);
+              }}
+              className="cursor-pointer"
+              isActive={isCurrentPath('/operation/inbound')}
+            >
+              Inbound
+            </SideNavLink>
+            <SideNavLink
+              renderIcon={PortOutput}
+              onClick={() => {
+                router.push(`${process.env.PATH_PREFIX}/operation/outbound`);
+              }}
+              className="cursor-pointer"
+              isActive={isCurrentPath('/operation/outbound')}
+            >
+              Outbound
+            </SideNavLink>
+            <SideNavLink
+              renderIcon={InventoryManagement}
+              onClick={() => {
+                router.push(`${process.env.PATH_PREFIX}/operation/stocktaking`);
+              }}
+              className="cursor-pointer"
+              isActive={isCurrentPath('/operation/stocktaking')}
+            >
+              Stocktaking
+            </SideNavLink>
+            <SwitcherDivider />
+            <SideNavLink
+              renderIcon={Analytics}
+              onClick={() => {
+                router.push(`${process.env.PATH_PREFIX}/analysis`);
+              }}
+              className="cursor-pointer"
+              isActive={isCurrentPath('/analysis')}
+            >
+              Analysis
+            </SideNavLink>
+            <SideNavLink
+              renderIcon={WatsonHealth3DCursor}
+              onClick={() => {
+                router.push(`${process.env.PATH_PREFIX}/analysis/3d`);
+              }}
+              className="cursor-pointer"
+              isActive={isCurrentPath('/analysis/3d')}
+            >
+              3D-Modeling
+            </SideNavLink>
+          </SideNavItems>
+        </SideNav>
+      </Theme>
       <HeaderGlobalBar className="flex items-center">
         <Toggle
           labelA="Light"
@@ -146,9 +149,17 @@ export const HeaderWSideNav = ({
           onToggle={(e) => {
             console.log(e);
             if (e) {
-              setTheme({ headerTheme: 'g100', contentTheme: 'g10' });
+              setTheme({
+                headerTheme: 'g100',
+                contentTheme: 'g10',
+                sideNavTheme: 'g90',
+              });
             } else {
-              setTheme({ headerTheme: 'white', contentTheme: 'white' });
+              setTheme({
+                headerTheme: 'white',
+                contentTheme: 'white',
+                sideNavTheme: 'white',
+              });
             }
           }}
         />
