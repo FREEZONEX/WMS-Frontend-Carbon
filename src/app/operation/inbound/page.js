af380b5a5050466ef65c6bce50bd2fdc477f278c
YangYue128-helen <yangyue1228@hotmail.com>
2024-03-28 18:24:11 +0800

    fix(router): change home page router

--- src/app/operation/inbound/page.js
+++ src/app/operation/inbound/page.js
@@ -116,7 +116,7 @@ function Page() {
         <BreadcrumbItem>
           <a
             onClick={() => {
-              router.push(`${process.env.PATH_PREFIX}/`);
+              router.push(`${process.env.PATH_PREFIX}/home`);
             }}
           >
             Home
