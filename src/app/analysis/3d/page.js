dd6ea9062a13c067e52c02e43d252e517a43a848
ShiMu <1239380544@qq.com>
2024-03-29 09:55:30 +0800

    new 3d datasource

--- src/app/analysis/3d/page.js
+++ src/app/analysis/3d/page.js
@@ -23,14 +23,19 @@ function page() {
         </div>
       </div>
       <div className="mt-10 flex items-center justify-center">
-        <iframe
+        {/* <iframe
           title="warehouse"
           width="1800"
           height="1120.5"
           src="https://app.powerbi.com/view?r=eyJrIjoiMzM1MjM5MmUtODY2OC00MzZiLWIyZDQtOTdiYmEwMzdlNTQwIiwidCI6ImFhNGU1ODM1LWU3YjctNDQ3NC1hZTE1LWQ3OTA0OTYwZDY2NCIsImMiOjEwfQ%3D%3D"
           frameborder="0"
           allowFullScreen="true"
-        ></iframe>
+        ></iframe> */}
+        <iframe title="newwarehouse" width="1800" height="1120.5" 
+        src="https://app.powerbi.com/view?r=eyJrIjoiMTA1OGRmNjgtMGVhYi00ZDNmLWI5YzYtZmUwODYzMzc0MDEyIiwidCI6ImFhNGU1ODM1LWU3YjctNDQ3NC1hZTE1LWQ3OTA0OTYwZDY2NCIsImMiOjEwfQ%3D%3D" 
+        frameborder="0" 
+        allowFullScreen="true">
+        </iframe>
       </div>
     </div>
   );
