dc00adcd2c0fbb84b70d12396616977e639ec459
hongzhi1234 <zhanghongzhi@freezonex.io>
2024-04-06 17:55:52 +0800

    add Dockerfile for wms frontend

--- next.config.js
+++ next.config.js
@@ -4,13 +4,27 @@ const nextConfig = {
     PATH_PREFIX: '',
   },
   async rewrites() {
+    let destination;
+
+    switch (process.env.RUNTIME_IDC_NAME) {
+      case 'local':
+        destination = 'http://127.0.0.1:8085/:slug*'; // Local development destination
+        break;
+      case 'sg':
+        destination = 'wms-server-service.wms/:slug*'; // Production destination in China
+        break;
+      case 'cn':
+        destination = 'http://office.unibutton.com:6585//:slug*'; // Example destination for Singapore
+        break;
+      default:
+        destination = 'http://47.236.10.165:30085/:slug*'; // Default or fallback destination
+    }
+
     return [
       {
         source: '/wmsbackendapi/:slug*',
-        destination: `http://office.unibutton.com:6585//:slug*`,
-        // destination: `http://localhost:8080/:slug*`,
+        destination: destination,
       },
-      //destination: `http://office.unibutton.com:6585//:slug*`,
     ];
   },
 };
