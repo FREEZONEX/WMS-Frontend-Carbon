20208b9e413f5176304a00a3ec96582de6f5245c
YangYue128-helen <yangyue1228@hotmail.com>
2024-04-12 17:42:47 +0800

    feat(ui): webgl

--- src/app/home/page.js
+++ src/app/home/page.js
@@ -18,14 +18,13 @@ export default function Home() {
       >
         Getting Started
       </Link>
-      <div className="mt-10 flex items-center justify-center">
+      <div className="mt-[-100px] flex items-center justify-center">
         <iframe
-          title="tuto_sketchup_warehouse"
+          title="webgl"
           width="1800"
           height="1120.5"
-          src="https://app.powerbi.com/reportEmbed?reportId=40841fb9-520d-4003-91ef-71c3ffddadef&autoAuth=true&ctid=aa4e5835-e7b7-4474-ae15-d7904960d664"
+          src="/MQTWebGl/index.html"
           frameborder="0"
-          allowFullScreen={true}
         ></iframe>
       </div>
     </>
