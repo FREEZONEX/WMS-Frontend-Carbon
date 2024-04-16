e6d3e48f4e603d3053e1cd4560311b2aeff571b5
YangYue128-helen <yangyue1228@hotmail.com>
2024-04-16 18:11:24 +0800

    feat(3d): webgl camera

--- src/components/WebGL/UnitiyWebGL.js
+++ src/components/WebGL/UnitiyWebGL.js
@@ -2,19 +2,24 @@ import React, { useEffect, useRef } from 'react';
 import { Unity, useUnityContext } from 'react-unity-webgl';
 
 const UnityWebGL = () => {
-  const buildUrl = '/MQTTWEBGL';
-  const { unityProvider } = useUnityContext({
+  const buildUrl = '/webgl/MQTTWEBGL';
+  const { unityProvider, requestFullscreen } = useUnityContext({
     loaderUrl: buildUrl + '.loader.js',
     dataUrl: buildUrl + '.data',
     frameworkUrl: buildUrl + '.framework.js',
     codeUrl: buildUrl + '.wasm',
   });
-
+  function handleClick() {
+    requestFullscreen(true);
+  }
   return (
-    <Unity
-      unityProvider={unityProvider}
-      style={{ height: '100%', width: '100%' }}
-    />
+    <>
+      <Unity
+        unityProvider={unityProvider}
+        style={{ height: '100%', width: '100%' }}
+      />
+      {/* <button onClick={handleClick}>Enter Fullscreen</button> */}
+    </>
   );
 };
 
