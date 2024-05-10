55fb429407902ba2634a255dde52cdc23b91a098
chenyiwen <chenyiwen@supcon.com>
2024-05-10 11:44:32 +0800

    feat(constant): update

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,3 +2,16 @@ export const DateTimeFormat = {
   shortDate: 'DD/MM/yyyy',
   longDate: 'DD/MM/yyyy HH:mm:ss',
 };
+
+export const tagColors = [
+  'red',
+  'magenta',
+  'purple',
+  'blue',
+  'cyan',
+  'teal',
+  'green',
+  'gray',
+  'cool-gray',
+  'outline',
+];
