cbf2d4bba08b87ea3b25c92dc2fbcab358f83d30
YangYue128-helen <yangyue1228@hotmail.com>
2024-06-19 16:25:31 +0800

    feat(unity): broker config json

--- src/components/WebGL/UnityWebGL.js
+++ src/components/WebGL/UnityWebGL.js
@@ -2,18 +2,28 @@ import React, { useEffect, useRef } from 'react';
 import { Unity, useUnityContext } from 'react-unity-webgl';
 
 const UnityWebGL = () => {
+  let brokerConfig = null;
+  const brokerDefault = {
+    type: 'wss',
+    address: 'supos.app',
+    port: '8084',
+    topicRequest: 'spBv2.0/wms/NCMD/unity/full/request',
+    topicIncrement: 'spBv2.0/wms/NDATA/server/increment',
+    topicResponse: 'spBv2.0/wms/NDATA/server/full/response',
+  };
+
   const buildUrl = '/webgl/WareHouseDemoWebGL_v3';
   const { unityProvider, requestFullscreen, sendMessage } = useUnityContext({
     // loaderUrl: buildUrl + '.loader.js',
     loaderUrl:
       'https://wmswebgl.oss-ap-southeast-1.aliyuncs.com/WareHouseDemoWebGL_v3.loader.js',
-    //buildUrl + '.data',
+    // dataUrl: buildUrl + '.data',
     dataUrl:
       'https://wmswebgl.oss-ap-southeast-1.aliyuncs.com/WareHouseDemoWebGL_v3.data',
     // frameworkUrl: buildUrl + '.framework.js',
     frameworkUrl:
       'https://wmswebgl.oss-ap-southeast-1.aliyuncs.com/WareHouseDemoWebGL_v3.framework.js',
-    //buildUrl + '.wasm',
+    // codeUrl: buildUrl + '.wasm',
     codeUrl:
       'https://wmswebgl.oss-ap-southeast-1.aliyuncs.com/WareHouseDemoWebGL_v3.wasm',
   });
@@ -21,18 +31,19 @@ const UnityWebGL = () => {
     requestFullscreen(true);
   }
   useEffect(() => {
+    fetch('https://wmswebgl.oss-ap-southeast-1.aliyuncs.com/config.json')
+      .then((res) => {
+        brokerConfig = res.json();
+        return brokerConfig;
+      })
+      .then((json) => console.log(json));
+
     if (typeof window !== 'undefined') {
       window.sendUnityMessage = function () {
-        const broker = {
-          type: 'wss',
-          address: 'supos.app',
-          port: '8084',
-        };
-
         sendMessage(
           'UnityWebServerFetcher',
           'GetServerInfo',
-          JSON.stringify(broker)
+          JSON.stringify(brokerConfig === null ? brokerDefault : brokerConfig)
         );
         console.log('Message received from Unity: ');
       };
